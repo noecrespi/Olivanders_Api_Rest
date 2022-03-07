9c0b5d306d307c7723e579dbd06749561fcd32b2
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-07 11:24:36 +0100

    [🍱] Mocks db

---
+++ repository/db.py
@@ -0,0 +1,8 @@
+class DB:
+    def init_db():
+        inventario = [
+            {"nombre": "Aged brie", "quality": 21, "sell_in": 20},
+            {"nombre": "Dexterity vest", "quality": 45, "sell_in": 30},
+            {"nombre": "Mana cake", "quality": 11, "sell_in": 40},
+        ]
+        return inventario
