1955800d9a6d97627819e5fd9b4d001108ab3968
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-14 16:22:01 +0100

    [✨] implementar App

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+""" Importación de los paquetes de flask """
 from flask import Flask
 from flask_restful import Resource, Api
+""" importación de los archivos del proyecto"""
 from controllers.inventario import Inventario
 from controllers.item import Item
 
+from repository.db import initApp
 
 app = Flask(__name__)
 api = Api(app)
+
+initApp(app)
 
 
 api.add_resource(Inventario,"/getAllItems")
