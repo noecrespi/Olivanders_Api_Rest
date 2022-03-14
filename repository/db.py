5d63029c98942688ecd8646e1d81c0c64d7075ac
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-14 16:22:24 +0100

    [✨] implementar BBDD Mongo

---
+++ repository/db.py
@@ -0,0 +1,89 @@
+from pydoc import cli
+from flask import g
+from mongoengine import connect
+from repository.models import Items
+""" Crea un comando """
+import click
+from flask.cli import with_appcontext
+
+
+
+defaultItems = [
+    {
+        "name": "Aged Brie",
+        "sell_in": 2,
+        "quality": 0,
+    },
+    {
+        "name": "+5 Dexterity Vest",
+        "sell_in": 10,
+        "quality": 20,
+    },
+    {
+        "name": "Elixir of the Mongoose",
+        "sell_in": 5,
+        "quality": 7,
+    },
+    {
+        "name": "Sulfuras, Hand of Ragnaros",
+        "sell_in": 0,
+        "quality": 80,
+    },
+    {
+        "name": "Sulfuras, Hand of Ragnaros",
+        "sell_in": -1,
+        "quality": 80,
+    },
+    {
+        "name": "Backstage Pass",
+        "sell_in": 15,
+        "quality": 20,
+    },
+    {
+        "name": "Backstage Pass",
+        "sell_in": 10,
+        "quality": 49,
+    },
+    {
+        "name": "Backstage Pass",
+        "sell_in": 5,
+        "quality": 49,
+    },
+    {
+        "name": "Conjured Mana Cake",
+        "sell_in": 3,
+        "quality": 6,
+    },
+]
+
+
+""" Da acceso a al abase de datos  """
+def getDb():
+    if 'db' not in g:
+        g.db = connect(
+            db = 'items',
+            host = 'mongodb+srv://items:items@items.ozyma.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+            )
+        g.Items = Items
+        return g.db
+
+""" Iniciar la base de datos y poblarlo """
+def initDb():
+    db = getDb()
+    for item in defaultItems:
+        Items(
+            name = item['name'],
+            sell_in = item['sell_in'],
+            quality = item['quality']
+        ).save() 
+        """ .save() : es una función guarda el objeto que hay en momoria en la base de dados  """
+
+@click.command('initDB')
+@with_appcontext
+def initDbConmmand():
+    initDb()
+    click.echo('La base de datos se ha poblado bien')
+
+def initApp(app):
+    app.cli.add_command(initDbConmmand)
+    
