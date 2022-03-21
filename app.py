e13e2962c845ffa908f3ca598bd13d2fb9476555
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-21 15:51:47 +0100

    [✨] implementar App

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 """ Importación de los paquetes de flask """
 from flask import Flask
 from flask_restful import Resource, Api
+from controllers.quality import Quality
+from controllers.updateQuality import UpdateQuality
+from controllers.sell_in import SellIn
 """ importación de los archivos del proyecto"""
 from controllers.inventario import Inventario
 from controllers.item import Item
@@ -15,5 +18,6 @@ initApp(app)
 
 api.add_resource(Inventario,"/getAllItems")
 api.add_resource(Item,"/items/<name>", "/items")
-
-
+api.add_resource(SellIn,"/sellin/<sellin>", "/sellin")
+api.add_resource(Quality,"/quality/<quality>", "/quality")
+api.add_resource(UpdateQuality,"/updateQuality")
