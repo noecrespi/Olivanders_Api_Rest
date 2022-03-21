fb3b2bb0196f39e6df61394725c7fa87fa5ec250
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-21 15:53:48 +0100

    [✨] implementar Service

--- service/Service.py
+++ service/Service.py
@@ -1,11 +1,10 @@
 from flask_restful import fields, marshal_with
 from repository.db import getDb
-from flask import g 
+from flask import g
+from domain.GildedRose import *
+from repository.models import Items 
 
 
-
-
-db =  []
 
 class Service:
     
@@ -16,6 +15,7 @@ class Service:
     }
 
     """ Devuelve todos los items, o sea toda la base de datos """
+    """ @marshal_with = a forma a la respuesta """
     @marshal_with(inventoryResourceFields)
     def getAllItems():
         db = getDb()
@@ -33,15 +33,65 @@ class Service:
             if name == item['name']:
                 inventario.append(item)
         return inventario
-    
+
+    """ Busca un nombre de un sell_in y te devuelve todo el valor """
+    @marshal_with(inventoryResourceFields)
+    def getBySellIn(sellIn):
+        db = getDb()
+        inventario =[]
+        for item in g.Items.objects():
+            if item['sell_in'] == int(sellIn):
+                inventario.append(item)
+        return inventario
+
+    """ Busca un nombre de un quality y te devuelve todo el valor """
+    @marshal_with(inventoryResourceFields)
+    def getByQuality(quality):
+        db = getDb()
+        inventario =[]
+        for item in g.Items.objects():
+            if item['quality'] == int(quality):
+                inventario.append(item)
+        return inventario
+
     """ Crear un item y lo añade"""
+    @marshal_with(inventoryResourceFields)
     def createItem(item):
-        db.append(item)
+        db = getDb()
+        db.insert_one(item)
+        return 'Se ha añadido correctamente el item'
 
     """ Eliminar un item """
     def deleteItem(name):
-        for item in db:
-            if name == item["nombre"]:
-                db.remove(item)
-                return db
-        return "No existe este item"
+        db = getDb()
+        message = 'No existe este item'
+        for item in g.Items.objects():
+            if name == item['name']:
+                item.delete()
+                message = 'Se han borrado los objetos'
+        return message
+        
+
+    """ Actualizar los items """
+    def updateQuality():
+        db = getDb()
+        classNames = {
+            'Backstage Pass' : 'Backstage',
+            'Conjured Mana Cake' : 'Conjured',
+            'Sulfuras, Hand of Ragnaros' : 'Sulfuras',
+            'Aged Brie' : 'AgedBrie'
+        }
+        for item in g.Items.objects():
+            itemTupla = [item['name'], item['sell_in'], item['quality']]
+            try:
+                className = classNames[item['name']]
+            except:
+                className = 'NormalItem'
+            
+            """ eval : encontar una clase que coincida en la string className """
+            pythonObject = eval(className + str(tuple(itemTupla)))
+            pythonObject.updateQuality()
+            item.sell_in = pythonObject.sellIn
+            item.quality = pythonObject.quality
+            item.save()
+        return Service.getAllItems()
