898e65a077e254d1084ca8cb38565c97bc6f8af8
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-03-25 19:38:24 +0100

    [:sparkles:] Implementado comprobación añadir item

--- controllers/item.py
+++ controllers/item.py
@@ -11,10 +11,25 @@ class Item(Resource):
     
     """ CREAR """
     def post(self):
-        args = request.get_json()
+        args = self.parseRequest()
         Service.createItem(args)
         return "Se ha añadido"
     
     """ Delete """
     def delete(self,name):
-        return Service.deleteItem(name)
+        return 
+
+    """ llamas la función ya la request, 
+    compruba si teiene las propiedades (tres, name, sell_in, quality) 
+    Dice el tipo de objeto que a de ser
+    Se puede marcar si queremos que algun valor sea obligatorio
+    envia un mensaje en el caso que algo falle"""
+    def parseRequest(self):
+        parser = reqparse.RequestParser(bundle_errors=True)
+        parser.add_argument('name', type=str, required=True,
+                            help='introduce el nombre')
+        parser.add_argument('sell_in', type=int, required=True,
+                            help='introduce un sellIn')
+        parser.add_argument('quality', type=int, required=True,
+                            help='introduce una quality')
+        return parser.parse_args()
